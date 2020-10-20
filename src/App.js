452f1f41378c36a68562389c36adba5fa9ac9319
willwearing <willwearing@gmail.com>
2020-10-19 19:30:02 -0600

    auth working correctly and now rendering user data on the private path

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
 import Dashboard from "./components/Dashboard";
 import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <div className="App">
       <h1>Stock Tracking App</h1>
       <LoginButton />
+      <LogoutButton />
       <Dashboard />
     </div>
   );
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import JSONPretty from "react-json-pretty";
 
 const Dashboard = () => {
+  const { user, isAuthenticated } = useAuth0();
+  console.log(isAuthenticated);
   return (
-    <div>
-      <h3>Here goes all the stock data</h3>
-    </div>
+    isAuthenticated && (
+      <div className="usercard">
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        {/* <JSONPretty data={user} /> */}
+      </div>
+    )
   );
 };
 
